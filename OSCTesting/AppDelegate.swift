21a97b1104de29194d2ee09d0ac0ed420a2a0596
GW Rodriguez <grodriguez@feldinc.com>
2020-01-17 13:10:55 -0500

    Initial Commit

---
+++ OSCTesting/AppDelegate.swift
@@ -0,0 +1,25 @@
+// ==================================================================
+// Created by:  GW Rodriguez
+// Date:        1/17/20
+// Swift:       5.0
+// Copyright:   Copyright © 2020 GW Rodriguez. All rights reserved.
+// ==================================================================
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OSCTesting/ViewController.swift
@@ -0,0 +1,26 @@
+// ==================================================================
+// Created by:  GW Rodriguez
+// Date:        1/17/20
+// Swift:       5.0
+// Copyright:   Copyright © 2020 GW Rodriguez. All rights reserved.
+// ==================================================================
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
