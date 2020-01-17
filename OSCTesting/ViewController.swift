7f06c19b33d641a467a84cee8faa35c75631ccb7
GW Rodriguez <grodriguez@feldinc.com>
2020-01-17 13:35:22 -0500

    basic functionality

--- OSCTesting/ViewController.swift
+++ OSCTesting/ViewController.swift
@@ -6,21 +6,54 @@
 // ==================================================================
 
 import Cocoa
+import SwiftOSC
 
-class ViewController: NSViewController {
+
+class ViewController: NSViewController, OSCServerDelegate {
+    
+    var server: OSCServer!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            
         }
     }
 
 
+    @IBAction func connectClicked(_ sender: Any) {
+        server = OSCServer(address: "localhost", port: 8080)
+        server.delegate = self
+        server.start()
+    }
+    
+    
+    @IBAction func queryClicked(_ sender: Any) {
+        print("Address: \(server.address)")
+        print("Port: \(server.port)")
+        print("Running: \(server.running)")
+        print("Delegate: \(String(describing: server.delegate))")
+        print("Self: \(String(describing: self))\n")
+    }
+    
+    
+    func didReceive(_ message: OSCMessage) {
+        print(message)
+    }
+    
+    
+    func didReceive(_ bundle: OSCBundle) {
+        print("Bundle:\n\(bundle)")
+    }
+    
+    
+    func didReceive(_ data: Data) {
+        print("Data:\n\(data)")
+    }
 }
 
